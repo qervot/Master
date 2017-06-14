9ef2e19cb39b73c361fdc0a2a112efcc22f9ba16
Ilyas <10sis@mail.ru>
2017-06-14 17:02:33 +0300

    Add files via upload

--- GameCanvas.java
+++ GameCanvas.java
@@ -3,40 +3,48 @@ package ru.geekbrains.games.game_circles;
 import javax.swing.*;
 import java.awt.*;
 
-public class GameCanvas extends JPanel {
+class GameCanvas extends JPanel {
 
-    private static final int SLEEPING_TIME =17;
+    private static final int SLEEPING_TIME = 17;
+
     private GameWindow gameWindow;
     private long lastTime;
 
-    GameCanvas(GameWindow gameWindow) {
-        gameWindow = gameWindow;
+    GameCanvas(GameWindow gameWindow){
+        this.gameWindow = gameWindow;
         lastTime = System.nanoTime();
-
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        long currentTime = System.nanoTime();
+        float deltaTime = (currentTime - lastTime) * 1e-9f;
+        lastTime = currentTime;
 
-    long currentTime = System.nanoTime();
-    float deltaTime = (currentTime - lastTime) * 1e-9f;
-    lastTime = currentTime;
-//        try {
-//            Thread.sleep(SLEEPING_TIME);
- //       }catch (InterruptedException e){
-//          e.printStackTrace();
-//        g.fillOval(0,0,100,100);
-        gameWindow.onDrawFrame(this,g,deltaTime);
-            try {
-                Thread.sleep(SLEEPING_TIME);
-            }catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        gameWindow.onDrawFrame(this, g, deltaTime);
+        try {
+            Thread.sleep(SLEEPING_TIME);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         repaint();
-
     }
 
-}
+    int getLeft(){
+        return 0;
+    }
 
+    int getRight(){
+        return getWidth() - 1;
+    }
+
+    int getTop(){
+        return 0;
+    }
+
+    int getBottom(){
+        return getHeight() - 1;
+    }
+}
--- GameWindow.java
+++ GameWindow.java
@@ -4,11 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameWindow extends JFrame {
+
     private static final int POS_X = 600;
     private static final int POS_Y = 200;
     private static final int WIDTH = 800;
-    private static final int HEIGHT =600;
-    private static final String TITLE = "CIRCLES";
+    private static final int HEIGHT = 600;
+    private static final String TITLE = "Circles";
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
@@ -17,45 +18,50 @@ public class GameWindow extends JFrame {
                 new GameWindow();
             }
         });
-
     }
 
-    private GameWindow(){
+    private Sprite[] sprites = new Sprite[10];
 
-
+    private GameWindow() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocation(POS_X, POS_Y);
         setSize(WIDTH, HEIGHT);
-        setResizable(false);
+//        setResizable(false);
         setTitle(TITLE);
         GameCanvas gameCanvas = new GameCanvas(this);
         add(gameCanvas);
+        initGame();
         setVisible(true);
-
     }
 
-    @Override
-    public void setTransferHandler(TransferHandler newHandler) {
-        super.setTransferHandler(newHandler);
+    private void initGame(){
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i] = new Ball();
+        }
     }
+
     private float time;
 
-    void onDrawFrame(GameCanvas gameCanvas, Graphics g, float deltaTime){
-//        float fps = 1f/deltaTime;
-//        time += deltaTime;
-//        if (time>=1f) {
-//        System.out.println(int ) fps);
-//        time = 0f;
-//        }
-        g.fillOval(0,0,100,100);
-//        try {
-//            Thread.sleep(SLEEPING_TIME);
-//        }catch (InterruptedException e){
-//            e.printStackTrace();
+    void onDrawFrame(GameCanvas gameCanvas, Graphics g, float deltaTime) {
+        float fps = 1f / deltaTime;
+        time += deltaTime;
+        if(time >= 1f) {
+            System.out.println((int) fps);
+            time = 0f;
         }
-
-
-
+        update(gameCanvas, deltaTime);
+        render(gameCanvas, g);
     }
 
-//
+    private void update(GameCanvas gameCanvas, float deltaTime){
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i].update(gameCanvas, deltaTime);
+        }
+    }
+
+    private void render(GameCanvas gameCanvas, Graphics g){
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i].render(gameCanvas, g);
+        }
+    }
+}
